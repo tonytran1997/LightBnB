fae8d92c2e081b83269aeed6338f3970726cf831
Tony Tran <tonytran1645@gmail.com>
2023-06-19 09:08:30 -0400

    Update functions

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -67,11 +67,11 @@ const addUser = function (user) {
   return pool
     .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
     .then((result) => {
-      console.log(result);
+      console.log(result)
       return result;
     })
     .catch((err) => {
-      console.log(err);
+      console.log(err)
       throw err;
     });
 };
@@ -84,8 +84,24 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-};
+  return pool
+    .query(`SELECT properties.*, reservations.id, reservations.start_date, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2`, [guest_id, limit])
+    .then((result) => {
+      console.log(result)
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err)
+      throw err;
+    });
+};    
 
 /// Properties
 
@@ -103,7 +119,7 @@ const getAllProperties = (options, limit = 10) => {
       return result.rows;
     })
     .catch((err) => {
-      console.log(err.message);
+      console.log(err);
     });
 };
 /**
