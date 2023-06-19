b225a554c54b026cb8c63612757b0be6321ae3f2
Tony Tran <tonytran1645@gmail.com>
2023-06-19 08:47:37 -0400

    Update functions

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -17,14 +17,22 @@ pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.l
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+    .query(`SELECT * 
+    FROM users 
+    WHERE email = $1`, [email])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        console.log(result.rows);
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
 /**
@@ -33,7 +41,21 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT *
+    FROM users 
+    WHERE id = $1`, [id])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
 /**
@@ -42,10 +64,16 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
+    .then((result) => {
+      console.log(result);
+      return result;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
 /// Reservations
