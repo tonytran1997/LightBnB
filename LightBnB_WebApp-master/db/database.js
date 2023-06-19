9dd0129aefaa0303aaa8edaf326589f1557b6e6c
Tony Tran <tonytran1645@gmail.com>
2023-06-19 10:39:53 -0400

    Update function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   password: '123',
   host: 'localhost',
   database: 'Lightbnb'
+  
 });
 
 pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
@@ -179,6 +180,7 @@ const getAllProperties = (options, limit = 10) => {
     })
     .catch((err) => {
       console.log(err);
+      throw err
     });
 };
 /**
@@ -187,10 +189,53 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id, 
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  let queryString = `
+  INSERT INTO properties (
+    owner_id, 
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  )
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`;
+
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err
+    });
 };
 
 module.exports = {
